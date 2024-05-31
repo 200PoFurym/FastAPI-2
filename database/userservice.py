955a343429a3fb3c085491944ce2605f8f0fcb4c
Marat <maratiknyam@gmail.com>
2024-05-31 19:03:33 +0500

    first commit

---
+++ database/userservice.py
@@ -0,0 +1,9 @@
+from database.models import User, UserAnswers, Questions, Result
+from datetime import datetime
+from database import get_db
+def get_all_users_db():
+    # Соединение с нашим БД
+    db = next(get_db())
+    users = db.query(User).all()
+
+    return users
